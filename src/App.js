87b1e414782943b4b685e1b371c83e88c6d10a12
9boogie <jzero.youngs89@gmail.com>
2021-01-14 15:20:28 -0800

    component life cycle

--- src/App.js
+++ src/App.js
@@ -2,16 +2,32 @@ import { render } from "@testing-library/react";
 import React from "react";
 
 class App extends React.Component{
+  constructor(props){
+    super(props)
+    console.log("Hello")
+  }
   state = {
     count: 0
   };
   add = () => {
-    console.log("add")
+    console.log("add");
+    this.setState(current => ({ count: current.count + 1 }))
   };
   minus = () => {
-    console.log("minus")
+    console.log("minus");
+    this.setState(current => ({ count: current.count - 1 }))
   };
+  componentDidMount(){
+    console.log("component rendered")
+  };
+  componentDidUpdate(){
+    console.log("I am updated!")
+  };
+  componentWillUnmount(){
+    console.log("Good Bye")
+  }
   render() {
+    console.log("I am rendering")
     return (
       <div>
         <h1>The number is {this.state.count}</h1>
